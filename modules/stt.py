7762cf394352300c3f769d2715efbcfe5a38c1b1
Gonzalo Mora <drgonzalomora@MacBook-Pro-3.local>
2026-03-07 11:22:36 +0100

    Actualizar dependencias y cambiar a modelo Parakeet v3 multilingüe

--- modules/stt.py
+++ modules/stt.py
@@ -3,7 +3,7 @@ from parakeet_mlx import from_pretrained
 # since it can take time, we print a message to the user
 print("Loading voice recognition model (please wait)...")
 
-model = from_pretrained("mlx-community/parakeet-tdt-0.6b-v2")
+model = from_pretrained("mlx-community/parakeet-tdt-0.6b-v3")
 
 
 def transcribe(audio_file_path):
